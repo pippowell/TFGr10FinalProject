f0a00afa78e2ba24a7cc02f3d481c322cb9732ad
Milad <miladrouygari@gmail.com>
2023-02-15 12:44:32 +0100

    NN model and cv2 script added

---
+++ cv2_mobilenet.py
@@ -0,0 +1,77 @@
+import cv2 as cv  # OpenCV computer vision library
+import numpy as np  # Scientific computing library
+import numpy as np
+import tensorflow as tf
+from tensorflow import keras
+import sys
+
+classes = ["background", "person", "bicycle", "car", "motorcycle",
+           "airplane", "bus", "train", "truck", "boat", "traffic light", "fire hydrant",
+           "unknown", "stop sign", "parking meter", "bench", "bird", "cat", "dog", "horse",
+           "sheep", "cow", "elephant", "bear", "zebra", "giraffe", "unknown", "backpack",
+           "umbrella", "unknown", "unknown", "handbag", "tie", "suitcase", "frisbee", "skis",
+           "snowboard", "sports ball", "kite", "baseball bat", "baseball glove", "skateboard",
+           "surfboard", "tennis racket", "bottle", "unknown", "wine glass", "cup", "fork", "knife",
+           "spoon", "bowl", "banana", "apple", "sandwich", "orange", "broccoli", "carrot", "hot dog",
+           "pizza", "donut", "cake", "chair", "couch", "potted plant", "bed", "unknown", "dining table",
+           "unknown", "unknown", "toilet", "unknown", "tv", "laptop", "mouse", "remote", "keyboard",
+           "cell phone", "microwave", "oven", "toaster", "sink", "refrigerator", "unknown",
+           "book", "clock", "vase", "scissors", "teddy bear", "hair drier", "toothbrush"]
+
+# Colors we will use for the object labels
+colors = np.random.uniform(0, 255, size=(len(classes), 3))
+
+# Open the webcam
+cam = cv.VideoCapture(0)
+
+# model (load weights and config)
+pb = 'frozen_inference_graph_mobile_net.pb'
+pbt = 'ssd_mobilenet_v1_coco_2017_11_17.pbtxt'
+
+# Read the neural network
+cvNet = cv.dnn.readNetFromTensorflow(pb, pbt)
+
+while True:
+
+    # Read in the frame
+    ret_val, img = cam.read()
+    rows = img.shape[0]
+    cols = img.shape[1]
+    cvNet.setInput(cv.dnn.blobFromImage(img, size=(300, 300), swapRB=True, crop=False))
+
+    # Run object detection
+    cvOut = cvNet.forward()
+
+    # Go through each object detected and label it
+    for detection in cvOut[0, 0, :, :]:
+        score = float(detection[2])
+        if score > 0.3:
+
+            idx = int(detection[1])  # prediction class index.
+
+            # Detecting what is needed! (introduce issues if all objects are detected)
+            if classes[idx] == 'apple' or classes[idx] == 'banana' or classes[idx] == 'orange':
+
+                left = detection[3] * cols
+                top = detection[4] * rows
+                right = detection[5] * cols
+                bottom = detection[6] * rows
+                cv.rectangle(img, (int(left), int(top)), (int(right), int(bottom)), (23, 230, 210), thickness=2)
+
+                # draw the prediction on the frame
+                label = "{}: {:.2f}%".format(classes[idx], score * 100)
+                y = top - 15 if top - 15 > 15 else top + 15
+                cv.putText(img, label, (int(left), int(y)), cv.FONT_HERSHEY_SIMPLEX, 0.5, colors[idx], 2)
+
+    # Display the frame
+    cv.imshow('my webcam', img)
+
+    # Press ESC to quit
+    if cv.waitKey(1) == 27:
+        break
+
+    # Stop filming
+cam.release()
+
+# Close down OpenCV
+cv.destroyAllWindows()
