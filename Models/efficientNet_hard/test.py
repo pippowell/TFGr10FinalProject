466ac2bc68bab9b172b7676968f14c7986f6d031
wlee <prizle858@gmail.com>
2023-03-23 12:56:49 +0100

    Create test.py

---
+++ Models/efficientNet_hard/test.py
@@ -0,0 +1,11 @@
+import tensorflow as tf
+from efficientNet import EfficientNet, phi_values
+
+def test():
+    version = "b0"
+    width_mul, depth_mul, res, dropout_rate = phi_values[version]
+    net = EfficientNet(version=version, num_classes=2)
+    x = tf.random.uniform(shape=[1, 3, res, res])
+    y = net(x)
+
+    print(y.size())
