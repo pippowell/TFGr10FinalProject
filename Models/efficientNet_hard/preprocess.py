7b1b7f2f5eb7acb38d577db1d28a7f626481a837
wlee <prizle858@gmail.com>
2023-03-30 12:25:33 +0200

    grape_images

--- Models/efficientNet_hard/preprocess.py
+++ Models/efficientNet_hard/preprocess.py
@@ -1,23 +1,35 @@
 import tensorflow_datasets as tfds
 import tensorflow as tf
+import splitfolders
+from pathlib import Path
 
-batch_size = 64
+directory = str(Path(__file__).parents[0]) + "/train&val&test/train"
 
-# The CIFAR-10 dataset consists of 60000 32x32 colour images in 10 classes, with 6000 images per class. There are 50000 training images and 10000 test images. 
-(train_ds, test_ds), ds_info = tfds.load('cifar10', split =['train', 'test'], as_supervised = True, with_info = True)
-NUM_CLASSES = ds_info.features["label"].num_classes
+# split the dataset into train, val, test dataset
+# splitfolders.ratio(f"str(Path(__file__).parents[0])/images/", output=f"{directory}/train&val&test", seed=1337, ratio=(.8, 0.1,0.1)) # A seed makes splits reproducible.
+
+# Define your data preprocessing pipeline
+image_size = (224, 224)
+batch_size = 32
+dataset = tf.keras.preprocessing.image_dataset_from_directory(
+    directory,
+    image_size=image_size,
+    batch_size=batch_size
+)
 
 def preprocess(dataset):
 
     # convert data from uint8 to float32
     dataset = dataset.map(lambda img, target: (tf.cast(img, tf.float32), target))
 
+    # flatten the images into vectors - we don't do this step for CNN, CNN layers expect standard image format input
+    # dataset = dataset.map(lambda img, target: (tf.reshape(img, (-1,)), target))
+
     # input normalization, just bringing image values from range [0, 255] to [-1, 1]
     dataset = dataset.map(lambda img, target: ((img / 128.) - 1., target))
 
-    # create one-hot targets with depth 10 since cifar 10 has 10 classes
+    # create one-hot targets with depth 2 since we need to distinguish grapes from non-grapes
     dataset = dataset.map(lambda img, target: (img, tf.one_hot(target, depth=10)))
-
     # cache
     dataset = dataset.cache()
 
@@ -29,8 +41,11 @@ def preprocess(dataset):
     # return preprocessed dataset # take 10 from 60000
     return dataset.take(10)
 
-train_dataset = preprocess(train_ds)
-test_dataset = preprocess(test_ds)
+
+train_dataset = preprocess(dataset)
+# val_dataset = preprocess(f"{directory}/train&val&test/val")
+# test_dataset = preprocess(f"{directory}/train&val&test/test")
+
 
 # checking
-# print(train_dataset)
+print(train_dataset)
