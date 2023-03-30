7c27fdbea067fccd21f0b0d20fc81637e2bda8ea
Piper Powell <ppowell@uni-osnabrueck.de>
2023-03-30 15:24:14 +0200

    add files for custom EN SSD

---
+++ Models/TF_OD_API_NeededFiles/ssd_efficientnet_custom_feature_extractor.py
@@ -0,0 +1,11 @@
+import tensorflow.compat.v1 as tf
+
+import ssd_meta_arch
+from object_detection.models import feature_map_generators
+from object_detection.models.keras_models import mobilenet_v2
+from object_detection.utils import ops
+from object_detection.utils import shape_utils
+
+class EfficientNetFeatureExtractor(
+    ssd_meta_arch.SSDKerasFeatureExtractor):
+  """SSD Feature Extractor using MobilenetV2 features."""
